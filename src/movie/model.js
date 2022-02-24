1ba1fd85ac93ec22a701da092b4e03c01f3b55ab
GlennPS <glennsculthorp@gmail.com>
2022-02-24 13:21:02 +0000

    working add

---
+++ src/movie/model.js
@@ -0,0 +1,20 @@
+
+const mongoose = require("mongoose");
+
+const movieSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+//says a title is required
+        unique: true,
+//says title must only exist once
+    },
+    actor: {
+        type: String,
+        default: "Not specified",
+    },
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
+
+module.exports = Movie;
